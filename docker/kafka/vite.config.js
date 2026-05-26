744cd73bd2c9992b4db2b921249afd85e254ae6d
Roman Schmitz <roman.schmitz@gmail.com>
2026-05-26 17:36:24 +0200

    Base path fixed for Github pages hosting.

--- docker/kafka/vite.config.js
+++ docker/kafka/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/tcp-kafka-tuning/kafka/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
