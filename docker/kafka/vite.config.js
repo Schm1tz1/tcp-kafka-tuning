6ce5ef947ac7d5813b1c79df181ba54e1123b9cc
Schm1tz1 <ro-man@web.de>
2026-06-03 21:27:01 +0200

    Cleanup, JSX file duplication removed.

--- docker/kafka/vite.config.js
+++ docker/kafka/vite.config.js
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/tcp-kafka-tuning/kafka/',
+  resolve: {
+    dedupe: ['react', 'react-dom', 'recharts'],
+  },
+  server: {
+    fs: { allow: ['../../../dashboards', '.'] },
+  },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
